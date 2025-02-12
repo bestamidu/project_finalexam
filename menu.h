f2fadf714135bb22a244573db7017b9a8b1b5a23
bestamidu <ynhu22887@gmail.com>
2025-02-12 14:11:45 +0700

    menu.h

---
+++ menu.h
@@ -0,0 +1,10 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include "datatype.h"  // Thêm dòng này để nhận diện Category
+
+void mainMenu();
+void manageCategory(Category mycategory[], int size);
+
+
+#endif
